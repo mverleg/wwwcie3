ccf6abed597c7c8779e8ba7c24e0013025855b44
rafiki <mark@rafiki>
2015-09-30 20:20:37 +0200

    start of chat structure

--- urls.py
+++ urls.py
@@ -1,5 +1,8 @@
+
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import chatz.urls
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^chat/', include(chatz.urls)),
 )
