f394ef20044dd533dc2d31052b5ad53f3062f540
rafiki <mark@rafiki>
2015-10-01 17:27:18 +0200

    placeholder post view/url, and example form

--- chatz/models.py
+++ chatz/models.py
@@ -4,16 +4,19 @@ from django.db import models
 
 
 class ChatTopic(models.Model):
-    name= models.CharField(max_length=20,unique=True)
+    name = models.SlugField(unique = True)
+
     def __str__(self):
         return self.name
 
 
 class ChatMessage(models.Model):
-    user= models.ForeignKey(AUTH_USER_MODEL)
-    time= models.DateTimeField(auto_now=True)
-    text =models.TextField()
+    user = models.ForeignKey(AUTH_USER_MODEL)
+    time = models.DateTimeField(auto_now=True)
+    text = models.TextField()
     topic = models.ForeignKey(ChatTopic)
+
     def __str__(self):
         return '{0:s} @ {1:s}'.format(str(self.user), str(self.topic.name))
+
 
--- chatz/views.py
+++ chatz/views.py
@@ -1,15 +1,31 @@
-from datetime import timedelta
-from django.shortcuts import render
 
-# Create your views here.
-from django.utils.timezone import now
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.http import require_POST
 from chatz.models import ChatMessage, ChatTopic
 
 
-def show_chat(request):
-	topic = ChatTopic.objects.get(name='wiskunde1')
-	messages = ChatMessage.objects.filter(topic=topic)
+def chat_show(request, topic_name = 'wiskunde1'):
+	try:
+		topic = ChatTopic.objects.get(name = topic_name)
+	except ChatTopic.DoesNotExist:
+		return HttpResponse('We do not have a discussion about "{0:s}" yet. {1:s}'.format(topic_name, 'You can create it in the admin panel.' if request.user.is_staff else ''))
+	messages = ChatMessage.objects.filter(topic = topic)
 	return render(request, 'show_chat.html', {
 		'messages': messages,
+		'topic': topic,
 	})
+
+
+@login_required
+@require_POST
+def chat_post(request):
+	topic_name = request.POST.get('topic', '')
+	try:
+		topic = ChatTopic.objects.get(name = topic_name)
+	except ChatTopic.DoesNotExist:
+		return HttpResponse('We do not have a discussion about "{0:s}" yet. {1:s}'.format(topic_name, 'You can create it in the admin panel.' if request.user.is_staff else ''))
+	return HttpResponse('ja oke je hoort nog van ons doeeei! {0}'.format(', '.join('{0:s}: {1:}'.format(k, v) for k, v in request.POST.items())))
+
 
